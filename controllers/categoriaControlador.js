512db8700fe543101408134dfd409c9e67ce81a7
JoseArH <josearavenahernandez@gmail.com>
2024-10-24 20:06:26 -0300

    JSON categorias

--- controllers/categoriaControlador.js
+++ controllers/categoriaControlador.js
@@ -1,44 +1,52 @@
-const Categoria = require('../models/Categorias');
-const categoriaServicio = require('../services/categoriaServicio');
+const Categoria = require("../models/Categorias");
+const categoriaServicio = require("../services/categoriaServicio");
 
 const obtenerCategorias = async (req, res) => {
   const categorias = await categoriaServicio.obtenerTodoslasCategorias();
-  res.render('categorias/index', { categorias });
+  res.render("categorias/index", { categorias });
+};
+
+const obtenerCategoriasJson = async (req, res) => {
+  const productos = await productoServicio.obtenerTodosLosProductos();
+  res.send(productos);
 };
 
 const crearCategoria = async (req, res) => {
   const { nombre, descripcion } = req.body;
-  await categoriaServicio.crearCategoria({ nombre, descripcion});
-  res.redirect('/categorias');
+  await categoriaServicio.crearCategoria({ nombre, descripcion });
+  res.redirect("/categorias");
 };
 
 const obtenerCategoria = async (req, res) => {
-  const categoria = await categoriaServicio.obtenerCategoriaPorId(req.params.id);
-  res.render('categorias/detalle', { categoria });
+  const categoria = await categoriaServicio.obtenerCategoriaPorId(
+    req.params.id
+  );
+  res.render("categorias/detalle", { categoria });
 };
 
 const eliminarCategoria = async (req, res) => {
   await categoriaServicio.eliminarCategoria(req.params.id);
-  res.redirect('/categorias');
+  res.redirect("/categorias");
 };
 
 const actualizarCategoria = async (req, res) => {
   await categoriaServicio.actualizarCategoria(req.params.id, req.body);
-  res.redirect('/categorias');
+  res.redirect("/categorias");
 };
 const mostrarFormularioEditarCategoria = async (req, res) => {
   const categoria = await Categoria.findByPk(req.params.id);
   if (!categoria) {
-    return res.status(404).send('Categoria no encontrado');
+    return res.status(404).send("Categoria no encontrado");
   }
-  res.render('categorias/editarCategoria', { categoria });
+  res.render("categorias/editarCategoria", { categoria });
 };
 
 module.exports = {
-    obtenerCategorias,
-    crearCategoria,
-    obtenerCategoria,
-    eliminarCategoria,
-    actualizarCategoria,
-    mostrarFormularioEditarCategoria
+  obtenerCategorias,
+  crearCategoria,
+  obtenerCategoria,
+  eliminarCategoria,
+  actualizarCategoria,
+  mostrarFormularioEditarCategoria,
+  obtenerCategoriasJson,
 };
--- routes/categoriasRutas.js
+++ routes/categoriasRutas.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const categoriaControlador = require('../controllers/categoriaControlador');
 
 router.get('/', categoriaControlador.obtenerCategorias);
+router.get("/json", categoriaControlador.obtenerCategoriasJson)
 router.post('/', categoriaControlador.crearCategoria);
 router.get('/:id', categoriaControlador.obtenerCategoria);
 router.post('/:id', categoriaControlador.eliminarCategoria);/* esto deberia ser delete pero method de html solo acepta get|post, refactorizar */
